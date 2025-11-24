0f455ea9455a20dacadd1618b208a3d5c0a794e6
Yurii Yaremchuk <yaremchuk.yurii@student.po.edu.pl>
2025-11-24 14:04:56 +0100

    następna funkcja - podwyzka_procentowa

--- src/percent_test.py
+++ src/percent_test.py
@@ -9,3 +9,13 @@ def procent_z_liczby():
 procent_z_liczby()
 if __name__ == "__main__":
     procent_z_liczby()
+    
+    
+    
+def podwyzka_procentowa():
+    liczba = float(input("Podaj liczbę: "))
+    procent = float(input("Podaj procent podwyżki: "))
+
+    wynik = liczba * (1 + procent / 100)
+
+    print(f"Po podwyżce o {procent}%: {wynik}")
